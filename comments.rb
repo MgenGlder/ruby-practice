aeb869f5b3a7ee3c206996e40bb82a5d88707de1
Kunle Oshiyoye <mgenglder@github.com>
2021-08-30 17:04:25 -0400

    initial

---
+++ comments.rb
@@ -0,0 +1,4 @@
+1.class     # => Fixnum: the number 1 is a Fixnum
+0.0.class   # => Float: floating-point numbers have class Float
+true.class  # => TrueClass: true is a singleton instance of TrueClass
+false.class # => FalseClass
