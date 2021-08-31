4784c340a75359bba484414d6ee85dcfe6208c7a
Kunle Oshiyoye <mgenglder@github.com>
2021-08-31 17:34:57 -0400

    Add new line to one of ruby scripts

--- arrays-and-iterators.rb
+++ arrays-and-iterators.rb
@@ -1,8 +1,8 @@
 a = [3, 2, 1]     # This is an array literal
 a[3] = a[2] - 1   # Use square brackets to query and set array elements
 a.each do |elt|   # each is an iterator. The block has parameter elt
-    print elt + 1 # Prints "4321"
-end               # This block was delimited with do/end instead of {}
+  print elt + 1 # Prints "4321"
+end             # This block was delimited with do/end instead of {}
 
 a = [1, 2, 3, 4]         # Start with an array
 b = a.map{|x| x*x}       # Square elements: b is [1, 4, 9, 16]
@@ -10,5 +10,5 @@ print b
 c = a.select{|x| x%2==0} # Select even elements: c is [2, 4]
 print c
 a.inject do |sum,x|      # Kind of similar to the 'reduce' function of JavaScript
-    sum + x
+  sum + x
 end
