3af5818a43d5c6c2de7a5b2220fe0709f5c16d60
Kunle Oshiyoye <mgenglder@github.com>
2021-08-31 11:34:26 -0400

    Refactor times print to use the correct conventions

---
+++ times_print_kunle.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+5.times do
+  print 'kunle'
+  print ' '
+end
+
+# Avoid using { } for multi-line blocks.
+# Use single quote ticks for strings
+# Use do-end for multi-line blocks
