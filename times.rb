34824cad7696822644e517cde407f53e784207ae
Kunle Oshiyoye <mgenglder@github.com>
2021-09-02 13:42:06 -0400

    Remove duplicate files

--- times.rb
+++
@@ -1,5 +0,0 @@
-3.times{ print "Ruby!" }
-
-# In order to run ruby commands: ruby times.rb
-
-1.upto(8) { |x| print x } # Invoking methods on integers.
--- times_print_is_frozen.rb
+++
@@ -1,3 +0,0 @@
-print 'some string'.frozen?
-print 'some other string'.freeze
-# when you use freeze you can force a string to be immutable. It's kind of cool actually.
