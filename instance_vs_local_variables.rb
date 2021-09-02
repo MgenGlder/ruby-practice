929049377ca0eb34128f5d4e9f1b93a817ecf0d1
Kunle Oshiyoye <mgenglder@github.com>
2021-09-02 14:00:02 -0400

    Local Variables / Instance Variables stuff

---
+++ instance_vs_local_variables.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module MainGuy
+  class SubGuy
+    class SubSubGuy
+      def initialize(name, *args)
+        @name = name # @ variables are instance level, means rails will make them available to the view
+        @args = args # Without it, they're called and considered, local variables.
+            # validate_args  <-- if you wanted to call a function this way, you could
+            # They don't always need to have () if they don't have any parameters
+        end
+    end
+  end
+end
+
