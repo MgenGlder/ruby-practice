93bc1ab89747692e28b0c25f12a95ac8113a8a2d
Kunle Oshiyoye <mgenglder@github.com>
2021-09-03 12:12:08 -0400

    Add parallel assignment

---
+++ The Ruby Programming Language/Chapter 1./operators_and_parallel_assignments.rb
@@ -0,0 +1,14 @@
+x = 1
+y = 1
+
+x += 1      # Ruby doesn't have a ++ operator either
+y -= 1      # Ruby doesn't have an -- operator
+
+puts x
+puts y
+
+
+# Parallel assignment
+x, y = 1, 2     
+a, b = b, a
+x, y, z = [1, 2, 3]
