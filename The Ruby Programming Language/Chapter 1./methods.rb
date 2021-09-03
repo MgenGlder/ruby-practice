3c95a6095a80bdc5e373e661e73f3dd5cc1c1558
Kunle Oshiyoye <mgenglder@github.com>
2021-09-03 10:29:59 -0400

    Singletonmethods

---
+++ The Ruby Programming Language/Chapter 1./methods.rb
@@ -0,0 +1,5 @@
+def square(x)       # Def is used to create a function
+    x*x             # When a method, like the one here is defined outside of a class or module, if is global.
+end
+
+# These types of global methods are called singletonmethods
