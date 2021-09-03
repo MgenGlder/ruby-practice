20942e779e0d062a381c4eadc1cccf315bd554bb
Kunle Oshiyoye <mgenglder@github.com>
2021-09-03 12:45:03 -0400

    Added a new function that returns a tuple (multiple outputs)

--- The Ruby Programming Language/Chapter 1./operators_and_parallel_assignments.rb
+++ The Ruby Programming Language/Chapter 1./operators_and_parallel_assignments.rb
@@ -14,3 +14,14 @@ a, b = b, a
 x, y, z = [1, 2, 3]
 
 puts x, y, z # Can pass multiple paramters to Ruby puts
+
+# Define a method to convert Cartesian (x, y) coordinates to Polar
+def polar(x, y)
+    theta = Math.atan2(y, x)    # Compute the angle
+    r = Math.hypot(x, y)        # Compute the distance
+    [r, theta]                  # The last expression is the return value
+end
+
+distance, angle = polar(2, 2)   # Actual parallel assignment
+
+puts distance, angle
