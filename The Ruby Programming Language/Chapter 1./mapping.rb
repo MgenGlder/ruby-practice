389cd557b2a469dae9789aeb4c3a0edc14fffc0a
Kunle Oshiyoye <mgenglder@github.com>
2021-09-21 23:17:32 -0400

    small change to not cause a collision

--- The Ruby Programming Language/Chapter 1./mapping.rb
+++ The Ruby Programming Language/Chapter 1./mapping.rb
@@ -12,7 +12,7 @@ end
   # Note: In Ruby, the map function doesn't mutate, it creates a new Array by default. Use the ! to make it destructive and mutative.
 
 
-class String
+class String2
     def toJadenCase
         self.split.map(&:capitalize).join(' ')
     end
