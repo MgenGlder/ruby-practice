a09b76eeedafe1506ef7d498393c86dd56ee5e25
Kunle Oshiyoye <mgen.glder@gmail.com>
2022-07-01 22:55:12 -0400

    Increment practice

--- The Ruby Programming Language/Chapter 1./comments.rb
+++ The Ruby Programming Language/Chapter 1./comments.rb
@@ -2,4 +2,4 @@
 0.0.class   # => Float: floating-point numbers have class Float
 true.class  # => TrueClass: true is a singleton instance of TrueClass
 false.class # => FalseClass
-# Times practiced: 2
+# Times practiced: 3
