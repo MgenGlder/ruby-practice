3607f6254b7f59dcf880619747035d7604248a15
Kunle Oshiyoye <mgenglder@github.com>
2022-08-24 16:41:09 -0400

    Add literals

---
+++ The Ruby Programming Language/Chapter 2./literals.rb
@@ -0,0 +1,9 @@
+=begin A literal
+Literals are values that appear directly in Ruby source code.
+The include numbers, strings of text, and regular expressions.
+=end
+1   # An integer literal
+1.0 # A floating-point literal
+'one' # A string literal
+"two" # Another string literal
+/three/ # A regular expression literal
