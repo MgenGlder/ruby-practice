30475ceaa78ccf7e1f13e483f9010f389e91ee7c
Kunle Oshiyoye <mgenglder@github.com>
2022-08-30 15:35:03 -0400

    Spaces and method invokation notes

---
+++ The Ruby Programming Language/Chapter 2./spaces_and_method_invokations.rb
@@ -0,0 +1,11 @@
+f(3+2)+1
+f (3+2)+1
+# Two different ways to invoke a method...
+# The first way is calling f with the argument 5
+# The second way is calling f with the argument 6 because the entire second expression is considered the arguement for function f
+=begin Some notes....
+So...
+Never put a space after the function name and before the first argument.
+If the first argument to a method begins wiht an open parenthesis, always use the parentheses in the method invokation.
+Always run the Ruby interpreter with the -w option.
+=end
