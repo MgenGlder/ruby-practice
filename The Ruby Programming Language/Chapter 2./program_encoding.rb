b815cac520ce755697bba096cd6d6a9553d27b44
Kunle Oshiyoye <mgenglder@github.com>
2022-09-21 23:39:20 -0400

    Add items

--- The Ruby Programming Language/Chapter 2./program_encoding.rb
+++ The Ruby Programming Language/Chapter 2./program_encoding.rb
@@ -3,3 +3,7 @@
 # vi: set fileencoding=utf-8 :
 # !/usr/bin/ruby -w
 # coding: utf-8
+
+=begin Rememeber!
+Note, by default, the Ruby interpreter assumes that programs are encoded in ASCII
+=end
---
+++ The Ruby Programming Language/Chapter 2./program_execution.rb
@@ -0,0 +1,5 @@
+=begin
+When the Ruby interpreter encounters a class definition, it executes it, causing
+a new class to come into existence. Similarly, when the Ruby interpreter encounters a method
+definition, it executes, causing a new method to be defined.
+=end
