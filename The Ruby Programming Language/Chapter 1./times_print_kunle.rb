7d50efec25c75c1cb73a1d1ad830d4fa4c3c6cd3
Kunle Oshiyoye <mgenglder@github.com>
2022-09-22 01:02:15 -0400

    Doing something multiple times and printing it. Slowly and surely.

--- The Ruby Programming Language/Chapter 1./times_print_kunle.rb
+++ The Ruby Programming Language/Chapter 1./times_print_kunle.rb
@@ -6,6 +6,10 @@
   print x
 end
 
+5.times do |x|
+  print 'some text that you want to print'
+end
+5.times { |x| print 'can also print some text like this' }
 # Avoid using { } for multi-line blocks.
 # Use single quote ticks for strings
 # Use do-end for multi-line blocks
