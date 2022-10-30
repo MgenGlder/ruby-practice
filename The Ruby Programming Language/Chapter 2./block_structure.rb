84cdaed92d01ea305478e3ec402f74d1b081eded
Kunle Oshiyoye <mgenglder@github.com>
2022-10-30 15:07:17 -0400

    Update block structure

--- The Ruby Programming Language/Chapter 2./block_structure.rb
+++ The Ruby Programming Language/Chapter 2./block_structure.rb
@@ -12,3 +12,17 @@ may be delimited with curly braces, or they may be delimited with do and end.
 =end
 
 # Bodies are never delimited with curly braces in Ruby- key-words usually serve as delimiters instead.
+
+# Body vs. Block
+=begin Separating body vs block
+module Stats                                    # A module
+    class Dataset                               # A class in the module
+        def initialize(filename)                # A method in the class
+            IO.foreach(filename) do |line|      # A block in the method
+                if line[0,1] == "#"             # An if statement in the block
+                    next                        # A simple statement in the if
+                end                             # End the if body
+            end                                 # End the block
+        end                                     # End the method body
+    end                                         # End the class body
+end                                             # End the module body
