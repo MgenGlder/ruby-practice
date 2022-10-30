f082acd853fbad77662c1f664c69ee0f7dab5194
Kunle Oshiyoye <mgenglder@github.com>
2022-10-30 15:54:04 -0400

    Add new information?

--- The Ruby Programming Language/Chapter 3./arithmentic_in_ruby.rb
+++ The Ruby Programming Language/Chapter 3./arithmentic_in_ruby.rb
@@ -13,3 +13,7 @@ known as NaN, or Not-A-Number.
 
 # Exponentiation can result in very large values. Remember that integers can become arbitrarily large, but _Float_ objets cannot represent numbers larger than _Float::Max_.
 # Thus, the expression 10**1000 yields an exact integer result, but the expression 9.9**1000 overflows to the Float value Infinity.
+
+# Consider the quotient -7/3. Ruby rounds toward negative infinity and returns -3. C and related languages round toward zero instead and return -2. In Ruby, -a/b and a/-b but may not equal -(a/b).
+# Ruby's definition of the modulo operation also differs from that of C and Java. In Ruby, -7%3 is 2. In C and Java, the result is -1 instead. The magnitude of the result differs, because the quotient
+# differed. But the sign of the result differs, too. In Ruby, the sign of the result is always the sign of the second operand. In C and Java, the sign of the result is always the sign of the first operand.
