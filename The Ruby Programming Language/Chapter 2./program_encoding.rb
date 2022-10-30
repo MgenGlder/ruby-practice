7db448026d09e954e7ee2f383eb0afec99eeead9
Kunle Oshiyoye <mgenglder@github.com>
2022-10-30 15:13:28 -0400

    Add more information about encoding

--- The Ruby Programming Language/Chapter 2./program_encoding.rb
+++ The Ruby Programming Language/Chapter 2./program_encoding.rb
@@ -7,3 +7,13 @@
 =begin Rememeber!
 Note, by default, the Ruby interpreter assumes that programs are encoded in ASCII
 =end
+
+=begin Default external and default internal encodings
+The source encoding is what we described earlier: it tells the Ruby interpreter how to read characters in a script.
+Source encodings are typically set with coding comments.
+The default external encoding is something different: this is the encoding that Ruby uses by default when reading from files and streams.
+The default external encoding is global to the Ruby process and does not change from file to file. Normally the default external encoding is set based
+on the locale that your computer is configured to.
+
+The default internal encoding does not affect the encoding of string literals, but it is quite important for I/O.
+=end
